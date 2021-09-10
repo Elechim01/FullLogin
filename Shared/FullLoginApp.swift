6edcf5abf0bc75e498c9c55c4cfe4ad1d8cba3e1
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-10 11:12:26 +0200

    Initial Commit

---
+++ Shared/FullLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullLoginApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct FullLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
